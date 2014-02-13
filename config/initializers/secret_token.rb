b8abcc65395a6e839c13a7256bc9af1823c0c9de
Travis Peterson <tekman82@yahoo.com>
2014-02-13 02:09:10 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fvsa::Application.config.secret_key_base = 'a3cae3f3c70cfe1ed418d12025effc130cbd3e59984e0034415bcd645df2c7c9c43c8c1d23fb47839e2b780239e844418005d5505f54772599f7c4fcb4d96d68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fvsa::Application.config.session_store :cookie_store, key: '_fvsa_session'
